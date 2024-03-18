e6d92cb732411c58ae13a7685ca02ccfdd436a7d
Farouk Zakaria <123017280+FaroukZakaria@users.noreply.github.com>
2024-03-18 17:27:44 +0200

    Changed SQLAlchemy usage, and separated models in their own file.

--- app.py
+++ app.py
@@ -4,17 +4,26 @@ Main app
 """
 
 from flask import Flask, render_template
-from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy import create_engine, Column, Integer, String
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker
+from models import User, Base
 
 # Initialize Flask app
 app = Flask(__name__)
 
 # Configure SQLAlchemy to connect to the database
-app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://demo:password@localhost/platform_data'
+engine = create_engine('mysql://public:password@localhost/platform_data')
 
+# Create the tables in the database
+Base.metadata.create_all(engine)
 
-# Initialize SQLAlchemy
-db = SQLAlchemy(app)
+# Create a sessionmaker to handle database sessions
+Session = sessionmaker(bind=engine)
+session = Session()
+
+#print(session.query(User).all())
+
 
 ##############################################################################
 
@@ -29,23 +38,5 @@ def index():
 # PLACEHOLDER FOR MORE ROUTES
 
 
-class User(db.Model):
-    """
-    User Model
-    """
-    id = db.Column(db.Integer, primary_key=True)
-    username = db.Column(db.String(80), unique=True, nullable=False)
-    email = db.Column(db.String(120), unique=True, nullable=False)
-
-    def __repr__(self):
-        return '<User %r>' % self.username
-
-
 if __name__ == "__main__":
-    try:
-        # Try to create a connection to the database
-        # db.create_all()
-        app.run(host='0.0.0.0', port=5000)
-    except Exception as e:
-        # If an error occurs, print the error message
-        app.logger.error("Error connecting to the database:", e)
+    app.run(host='0.0.0.0', port=5000)
---
+++ models.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+"""
+Created models are here
+"""
+
+
+from sqlalchemy import Column, Integer, String
+from sqlalchemy.ext.declarative import declarative_base
+
+
+Base = declarative_base()
+
+# Define your SQLAlchemy models
+class User(Base):
+    __tablename__ = 'users'
+
+    id = Column(Integer, primary_key=True)
+    firstname = Column(String(60), unique=True, nullable=False)
+    lastname = Column(String(60), unique=True, nullable=False)
+    email = Column(String(120), unique=True, nullable=False)
+
+    def __repr__(self):
+        return "<User(id={}, first name={}, last name={}, email={})>".format(
+                self.id,
+                self.firstname,
+                self.lastname,
+                self.email
+                )
