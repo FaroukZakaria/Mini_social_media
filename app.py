15ae2ef10776315f9356e683de1d8f7b4848691e
FaroukZakaria <GamesWithoutLose97@gmail.com>
2024-03-18 15:19:49 +0000

    done changing sqlalchemy and separated models in a file

--- app.py
+++ app.py
@@ -7,6 +7,7 @@ from flask import Flask, render_template
 from sqlalchemy import create_engine, Column, Integer, String
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
+from models import User, Base
 
 # Initialize Flask app
 app = Flask(__name__)
@@ -14,24 +15,15 @@ app = Flask(__name__)
 # Configure SQLAlchemy to connect to the database
 engine = create_engine('mysql://public:password@localhost/platform_data')
 
-
-# Define a declarative base
-Base = declarative_base()
-
-
-# Define your SQLAlchemy models
-class User(Base):
-    __tablename__ = 'users'
-
-    id = Column(Integer, primary_key=True)
-    username = Column(String(80), unique=True, nullable=False)
-    email = Column(String(120), unique=True, nullable=False)
-
-    def __repr__(self):
-        return f'<User(id={self.id}, username={self.username}, email={self.email})>'
-
 # Create the tables in the database
 Base.metadata.create_all(engine)
+
+# Create a sessionmaker to handle database sessions
+Session = sessionmaker(bind=engine)
+session = Session()
+
+#print(session.query(User).all())
+
 
 ##############################################################################
 
