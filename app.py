2db95a80b0498152db8c2f0c7f930d94e143d2aa
FaroukZakaria <GamesWithoutLose97@gmail.com>
2024-03-18 19:36:27 +0000

    Updated routes. Programmed signup button to create user and flash a success message after redirection to home.

--- app.py
+++ app.py
@@ -3,14 +3,23 @@
 Main app
 """
 
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect, url_for, flash
 from sqlalchemy import create_engine, Column, Integer, String
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 from models import User, Base
+import hashlib
+import secrets
+import string
 
 # Initialize Flask app
 app = Flask(__name__)
+
+# Generate a random string of ASCII letters, digits, and punctuation
+secret_key = ''.join(secrets.choice(string.ascii_letters + string.digits + string.punctuation) for _ in range(32))
+
+# Set the Flask application's secret key
+app.secret_key = secret_key
 
 # Configure SQLAlchemy to connect to the database
 engine = create_engine('mysql://public:password@localhost/platform_data')
@@ -21,8 +30,6 @@ Base.metadata.create_all(engine)
 # Create a sessionmaker to handle database sessions
 Session = sessionmaker(bind=engine)
 session = Session()
-
-#print(session.query(User).all())
 
 
 ##############################################################################
@@ -42,11 +49,38 @@ def login():
     """
     return render_template('login.html')
 
-@app.route('/signup', strict_slashes=False)
+@app.route('/signup', methods=['GET', 'POST'], strict_slashes=False)
 def signup():
     """
     Signup page
     """
+    if request.method == 'POST':
+        print("starting")
+        firstname = request.form['fullname']
+        print("first name")
+        lastname = request.form['username']
+        print("last name")
+        email = request.form['email']
+        password = request.form['password']
+
+        # Hash the password using hashlib
+        hashed_password = hashlib.sha256(password.encode()).hexdigest()
+
+        new_user = User(
+                firstname=firstname,
+                lastname=lastname,
+                email=email,
+                password=hashed_password
+                )
+        # Adding new user to database
+        session.add(new_user)
+        session.commit()
+
+        # Flash a success message
+        flash('Account created successfully!', 'success')
+
+        return redirect(url_for('index'))
+
     return render_template('signup.html')
 
 @app.route('/about', strict_slashes=False)
