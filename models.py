bdd9fcf50408ec7041eb1ef87e77579c16f609d0
FaroukZakaria <GamesWithoutLose97@gmail.com>
2024-03-23 22:16:05 +0000

    Improved relationship of author attribute

--- models.py
+++ models.py
@@ -4,8 +4,9 @@ Created models are here
 """
 
 
-from sqlalchemy import Column, Integer, String
+from sqlalchemy import Column, Integer, String, ForeignKey
 from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import relationship
 
 
 Base = declarative_base()
@@ -20,6 +21,8 @@ class User(Base):
     email = Column(String(120), unique=True, nullable=False)
     password = Column(String(120), nullable=False)
     is_admin = False
+    # Define the one-to-many relationship with Post
+    user_posts = relationship('Post', back_populates='author')
 
     def __repr__(self):
         return "<User(id={}, first name={}, last name={}, email={})>".format(
@@ -31,13 +34,14 @@ class User(Base):
 
 class Post(Base):
      __tablename__ = 'posts'
-     
+
      id = Column(Integer, primary_key=True)
      content = Column(String(60), nullable=False)
-     author = Column(String(60), nullable=False)
-     
+     author_id = Column(Integer, ForeignKey('users.id'), nullable=False)
+     author = relationship("User", back_populates="user_posts")
+
      def __repr__(self):
-         return "<Post(id={}, author={})>".format(
+         return "<Post(id={}, content={}, author={})>".format(
                 self.id,
                 self.content,
                 self.author
