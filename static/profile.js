10a9ea0a93451dfc00aef0f5d9c6c118654223e0
Farouk Zakaria <123017280+FaroukZakaria@users.noreply.github.com>
2024-03-26 01:31:04 +0200

    Better comment

--- static/profile.js
+++ static/profile.js
@@ -1,4 +1,4 @@
-// Add your JavaScript code for profile.html here
+// Code for the button that changes the first name
 document.addEventListener('DOMContentLoaded', function() {
     const changeNameBtn = document.getElementById('change-name-btn');
     const changeNameForm = document.getElementById('change-name-form');
