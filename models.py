c5cc29260f9fecd10aacc41d59570993f72be9af
FaroukZakaria <GamesWithoutLose97@gmail.com>
2024-03-28 20:52:01 +0000

    Added the Comment model and the CommentLike model. Added missing model representations.

--- models.py
+++ models.py
@@ -25,6 +25,9 @@ class User(Base):
     user_posts = relationship('Post', back_populates='author')
     user_likes = relationship('Like', back_populates='user')
 
+    user_comments = relationship("Comment", back_populates="author")
+    user_comment_likes = relationship("CommentLike", back_populates="user")
+
 
     def __repr__(self):
         return "<User(id={}, first name={}, last name={}, email={})>".format(
@@ -43,6 +46,9 @@ class Post(Base):
      author = relationship("User", back_populates="user_posts")
      likes = Column(Integer, default=0)
      post_likes = relationship('Like', back_populates="post")
+     post_comments = relationship("Comment", back_populates="post")
+     post_comment_likes = relationship("CommentLike", back_populates="post")
+
 
      def __repr__(self):
          return "<Post(id={}, content={}, author={})>".format(
@@ -61,3 +67,53 @@ class Like(Base):
 
     user = relationship("User", back_populates="user_likes")
     post = relationship("Post", back_populates="post_likes")
+
+    def __repr__(self):
+        return "<Like(id={}, user={}, post={})>".format(
+                self.id,
+                self.user,
+                self.post
+                )
+
+
+class Comment(Base):
+    __tablename__ = 'comments'
+
+    id = Column(Integer, primary_key=True)
+    content = Column(String(60), nullable=False)
+    author_id = Column(Integer, ForeignKey('users.id'), nullable=False)
+    post_id = Column(Integer, ForeignKey('posts.id'), nullable=False)
+    likes = Column(Integer, default=0)
+
+    author = relationship("User", back_populates="user_comments")
+    post = relationship("Post", back_populates="post_comments")
+    comment_likes = relationship("CommentLike", back_populates="comment")
+
+    def __repr__(self):
+        return "<Comment(id={}, content={}, author={}, post={})>".format(
+                self.id,
+                self.content,
+                self.author,
+                self.post
+                )
+
+
+class CommentLike(Base):
+    __tablename__ = 'comment_likes'
+
+    id = Column(Integer, primary_key=True)
+    user_id = Column(Integer, ForeignKey('users.id'), nullable=False)
+    comment_id = Column(Integer, ForeignKey('comments.id'), nullable=False)
+    post_id = Column(Integer, ForeignKey('posts.id'), nullable=False)
+
+    user = relationship("User", back_populates="user_comment_likes")
+    post = relationship("Post", back_populates="post_comment_likes")
+    comment = relationship("Comment", back_populates="comment_likes")
+
+    def __repr__(self):
+        return "<CommentLike(id={}, user={}, comment={}, post={})>".format(
+                self.id,
+                self.user,
+                self.comment,
+                self.post
+                )
